9187398f403ade243349c24e92d17af8b5b9a813
ThiagoHSAl <thiagohenriquesilva.a@gmail.com>
2024-09-09 19:58:58 -0300

    Create soma.c

---
+++ NEPS/soma.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // Lendo a entrada do exercício
+	int X, Y;
+	scanf("%d",&X);
+	scanf("%d",&Y);
+
+    X+=Y;
+    printf("%d",X);
+
+    return 0;
+}
