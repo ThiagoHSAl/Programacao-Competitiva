fd9ad61bb74245fad4affc70f4ff079a24b18bab
ThiagoHSAl <thiagohenriquesilva.a@gmail.com>
2024-09-09 20:01:23 -0300

    Create media_inteira.c

---
+++ NEPS/media_inteira.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // Lendo a entrada do exercício
+	int A, B;
+	scanf("%d",&A);
+	scanf("%d",&B);
+
+    A=(A+B)/2;
+    printf("%d",A);
+
+    return 0;
+}
