807f5c60db60c868d8028864672f969f8402ae55
ThiagoHSAl <thiagohenriquesilva.a@gmail.com>
2024-09-09 20:01:04 -0300

    Create soma_facil.c

---
+++ NEPS/soma_facil.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // Lendo a entrada do exercício
+	int A, B;
+	scanf("%d",&A);
+	scanf("%d",&B);
+
+    A+=B;
+    printf("%d",A);
+    
+    return 0;
+}
